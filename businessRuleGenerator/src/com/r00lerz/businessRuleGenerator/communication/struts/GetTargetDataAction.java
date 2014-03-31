10467bf65bcd2865a48cf06f79562502b9a76270
Mark <mark.van.lagen@gmail.com>
2014-03-31 15:17:40 +0200

    Create GetTargetData action

---
+++ businessRuleGenerator/src/com/r00lerz/businessRuleGenerator/communication/struts/GetTargetDataAction.java
@@ -0,0 +1,21 @@
+package com.r00lerz.businessRuleGenerator.communication.struts;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class GetTargetDataAction extends ActionSupport{
+	
+	//names of parameters here
+	
+		@Override
+		public String execute(){
+			//action here
+			return ActionSupport.SUCCESS;
+		}
+		
+		@Override
+		public void validate() {
+			//validation rules here
+		}
+		
+		//seters/getters for parameters here
+}
