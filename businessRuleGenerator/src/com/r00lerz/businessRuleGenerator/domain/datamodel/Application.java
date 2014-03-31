95aaa3244df8d76677a6a84ed1f33012b11a1cb2
smartlapus <smartlapus@gmail.com>
2014-03-31 12:04:48 +0200

    Brap

--- businessRuleGenerator/src/com/r00lerz/businessRuleGenerator/domain/datamodel/Application.java
+++ businessRuleGenerator/src/com/r00lerz/businessRuleGenerator/domain/datamodel/Application.java
@@ -1,5 +1,14 @@
 package com.r00lerz.businessRuleGenerator.domain.datamodel;
 
+import java.util.HashMap;
+
 public class Application {
+	
+	private String brgNaam;
+	private String appNaam;
+	
+	public void generateRule(HashMap businessRule){
+		//
+	}
 
 }
--- businessRuleGenerator/src/com/r00lerz/businessRuleGenerator/domain/datamodel/BusinessRule.java
+++ businessRuleGenerator/src/com/r00lerz/businessRuleGenerator/domain/datamodel/BusinessRule.java
@@ -1,5 +1,9 @@
 package com.r00lerz.businessRuleGenerator.domain.datamodel;
 
 public class BusinessRule {
+	
+	private String name;
+	private String description;
+	private String condition;
 
 }
