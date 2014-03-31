fee93676629e7ec4ddf09d8c56e7cf48520ab98e
Robin Altena <altenarobin@gmail.com>
2014-03-31 12:21:16 +0200

    defined basic layout for the struts actions

--- businessRuleGenerator/src/com/r00lerz/businessRuleGenerator/communication/struts/ActiveRuleAction.java
+++ businessRuleGenerator/src/com/r00lerz/businessRuleGenerator/communication/struts/ActiveRuleAction.java
@@ -1,5 +1,22 @@
 package com.r00lerz.businessRuleGenerator.communication.struts;
 
-public class ActiveRuleAction {
+import com.opensymphony.xwork2.ActionSupport;
+
+public class ActiveRuleAction extends ActionSupport{ //implements UserAware
+	
+	//names of parameters here
+	
+	@Override
+	public String execute(){
+		//action here
+		return ActionSupport.SUCCESS;
+	}
+	
+	@Override
+	public void validate() {
+		//validation rules here
+	}
+	
+	//seters/getters for parameters here
 
 }
--- businessRuleGenerator/src/com/r00lerz/businessRuleGenerator/communication/struts/DefineNewRuleAction.java
+++ businessRuleGenerator/src/com/r00lerz/businessRuleGenerator/communication/struts/DefineNewRuleAction.java
@@ -1,5 +1,21 @@
 package com.r00lerz.businessRuleGenerator.communication.struts;
 
-public class DefineNewRuleAction {
+import com.opensymphony.xwork2.ActionSupport;
 
+public class DefineNewRuleAction extends ActionSupport{
+	
+	//names of parameters here
+	
+		@Override
+		public String execute(){
+			//action here
+			return ActionSupport.SUCCESS;
+		}
+		
+		@Override
+		public void validate() {
+			//validation rules here
+		}
+		
+		//seters/getters for parameters here
 }
