8c4f5d083dce45bff24367b7614655b302fabb6e
smartlapus <smartlapus@gmail.com>
2014-04-03 15:21:32 +0200

    Removing shizzle

--- businessRuleGenerator/src/com/r00lerz/businessRuleGenerator/domain/datamodel/BackEndRuleType.java
+++
@@ -1,7 +0,0 @@
-package com.r00lerz.businessRuleGenerator.domain.datamodel;
-
-public class BackEndRuleType {
-
-	private String name;
-	
-}
--- businessRuleGenerator/src/com/r00lerz/businessRuleGenerator/domain/datamodel/Language.java
+++
@@ -1,5 +0,0 @@
-package com.r00lerz.businessRuleGenerator.domain.datamodel;
-
-public class Language {
-	private String name;
-}
