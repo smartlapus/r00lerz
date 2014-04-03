8723e8a2abe35f379853b37db9c0f7f5c2a6882e
Robin Altena <altenarobin@gmail.com>
2014-04-03 15:20:59 +0200

    added method to value to get its entity or attribute name

--- businessRuleGenerator/src/com/r00lerz/businessRuleGenerator/domain/datamodel/Value.java
+++ businessRuleGenerator/src/com/r00lerz/businessRuleGenerator/domain/datamodel/Value.java
@@ -14,4 +14,30 @@ public class Value {
 	public String toString() {
 		return value;
 	}
+	
+	protected String parseEntityName(){
+		//TODO::Check if valueType = dynamic
+		String entity = value.split("\\.")[0];
+		return entity;
+	}
+	
+	protected String parseAttributeName(){
+		//TODO::Check if valueType = dynamic
+		String attribute = value.split("\\.")[1];
+		return attribute;
+	}
+	
+	protected String abbreviateEntityName(){
+		return parseEntityName().substring(0,3).toUpperCase();
+	}
+	
+	protected String abbreviateAttributeName(){
+		return parseAttributeName().substring(0,3).toUpperCase();
+	}
+
+	public static void main(String[]args){
+		Value v = new Value("Entity.Attribute");
+		
+		System.out.println(v.abbreviateEntityName());
+	}
 }
