c81ceb602b77483fde40e5e778af7cb4064a77a8
smartlapus <smartlapus@gmail.com>
2014-04-03 15:45:59 +0200

    Restoring all java files in datamodel and hbm.xmls

---
+++ businessRuleGenerator/src/Launcher.java
@@ -0,0 +1,39 @@
+import java.math.BigDecimal;
+
+
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+
+import com.r00lerz.businessRuleGenerator.domain.datamodel.Operator;
+
+
+
+public class Launcher {
+
+public static void main(String[]args){
+
+		Operator operator = new Operator("aoksd");
+		
+		
+			
+		Configuration configuration = new Configuration().configure("hibernate.cfg.xml");
+		StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder().
+		applySettings(configuration.getProperties());
+		SessionFactory factory = configuration.buildSessionFactory(builder.build());
+		
+		Session s = factory.openSession();
+		Transaction tx = s.beginTransaction();
+		
+		s.save(operator);  //	This would save the object in session
+		s.flush();  	//SQL Query is generated
+		tx.commit();	//Commits the transaction
+		s.close();		//Session is closed
+		
+		
+	}
+
+}
