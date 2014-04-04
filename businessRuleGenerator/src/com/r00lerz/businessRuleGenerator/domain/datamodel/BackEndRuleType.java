186853e74d23b3ff15b956dc51e5e801742b48b7
Robin Altena <altenarobin@gmail.com>
2014-04-04 02:52:18 +0200

    Last small one. Now for application and businessRule

--- businessRuleGenerator/src/com/r00lerz/businessRuleGenerator/domain/datamodel/BackEndRuleType.java
+++ businessRuleGenerator/src/com/r00lerz/businessRuleGenerator/domain/datamodel/BackEndRuleType.java
@@ -1,7 +1,30 @@
 package com.r00lerz.businessRuleGenerator.domain.datamodel;
 
 public class BackEndRuleType {
-
+	private int id;
 	private String name;
+	
+	public BackEndRuleType(){}
+	
+	public BackEndRuleType(String name){
+		this.name = name;
+	}
+
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
 	
 }
