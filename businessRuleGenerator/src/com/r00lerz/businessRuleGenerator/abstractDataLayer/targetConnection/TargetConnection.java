5626e710fa0829e7f178130a5fcbebdf330b1e9e
Mark <mark.van.lagen@gmail.com>
2014-04-04 16:15:09 +0200

    YOLO Bug fix 2

--- businessRuleGenerator/src/com/r00lerz/businessRuleGenerator/abstractDataLayer/targetConnection/TargetConnection.java
+++ businessRuleGenerator/src/com/r00lerz/businessRuleGenerator/abstractDataLayer/targetConnection/TargetConnection.java
@@ -1,6 +1,11 @@
 package com.r00lerz.businessRuleGenerator.abstractDataLayer.targetConnection;
 
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Map;
+
 public interface TargetConnection {
 	public void persist();
-	public void getTargetData();
+	public Map<String, ArrayList<String>> getTargetData() throws SQLException;
+	public void close() throws SQLException;
 }
