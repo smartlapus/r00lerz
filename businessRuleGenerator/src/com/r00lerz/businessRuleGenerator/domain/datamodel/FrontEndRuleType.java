e970232076afa0e178f9c2cbfa1622a8f4467927
Robin Altena <altenarobin@gmail.com>
2014-04-04 02:16:55 +0200

    added frontEndRuleType to hibernate

--- businessRuleGenerator/src/com/r00lerz/businessRuleGenerator/domain/datamodel/FrontEndRuleType.java
+++ businessRuleGenerator/src/com/r00lerz/businessRuleGenerator/domain/datamodel/FrontEndRuleType.java
@@ -1,8 +1,11 @@
 package com.r00lerz.businessRuleGenerator.domain.datamodel;
 
 public class FrontEndRuleType {
+	private int id;
 	private String name;
 	private String abbreviation;
+	
+	public FrontEndRuleType(){}
 	
 	public FrontEndRuleType(String frontEndRuleType) {
 		this.name = frontEndRuleType;
@@ -28,5 +31,25 @@ public class FrontEndRuleType {
 	
 	public String getAbbreviation(){
 		return abbreviation;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setAbbreviation(String abbreviation) {
+		this.abbreviation = abbreviation;
 	}
 }
