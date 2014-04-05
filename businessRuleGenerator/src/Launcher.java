67982e5733e9078e14f339ead9742b4b7e019094
smartlapus <smartlapus@gmail.com>
2014-04-05 11:38:13 +0200

    Updated hibernate url to remove "warning".

--- businessRuleGenerator/src/Launcher.java
+++ businessRuleGenerator/src/Launcher.java
@@ -13,100 +13,12 @@ import com.r00lerz.businessRuleGenerator.domain.datamodel.Application;
 import com.r00lerz.businessRuleGenerator.domain.datamodel.BusinessRule;
 
 public class Launcher {
-	private static SessionFactory factory;
 	
-	public static SessionFactory getSessionFactory(){
-		if (factory == null){
-			try {
-				factory = new Configuration().configure().buildSessionFactory();
-			} catch (Throwable ex) {
-				System.err.println("Failed to create sessionFactory object." + ex);
-				throw new ExceptionInInitializerError(ex);
-			}
-		}
-		return factory;
-	}
-	
-	@Transactional
 	public static void main(String[] args) {
-
-		ArrayList<String> rhsValues = new ArrayList<String>();
-		rhsValues.add("18");
-		BusinessRule br = new BusinessRule("bla_bla_bla_01", "person.age must be equal to 18", "person.age", "must be equal to", rhsValues, "Attribute compare rule", "some generated code here");
-	
-		
-		
-//		BackEndRuleType persistable = new BackEndRuleType("");
-
-		// Mockito
-
-
-		try {
-			factory = new Configuration().configure().buildSessionFactory();
-		} catch (Throwable ex) {
-			System.err.println("Failed to create sessionFactory object." + ex);
-			throw new ExceptionInInitializerError(ex);
-		}
-		
-		Session session = factory.openSession();	
-		Transaction tx = null;
-		try{
-			
-			tx = session.beginTransaction();
-			Application app = (Application) session.get(Application.class, 1);
-			
-			app.getBusinessRules().add(br);
-			session.update(app);
-			session.flush(); // SQL Query is generated
-			tx.commit();
-		}catch (HibernateException e) {
-			if (tx!=null) tx.rollback();
-			e.printStackTrace();
-		}finally {
-			session.close();
-		}
-		
-//		Configuration configuration = new Configuration()
-//				.configure("hibernate.cfg.xml");
-//		StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder()
-//				.applySettings(configuration.getProperties());
-//		SessionFactory factory = configuration.buildSessionFactory(builder
-//				.build());
-//		Session s = factory.openSession();
-//		Transaction tx = s.beginTransaction();
+//		3602 ERROR SchemaUpdate       - HHH000388: Unsuccessful: alter table THO7_2013_2A_TEAM4.BRG_GENERATED_CODE add constraint FK_2xluaiv9br64gpiuoqyf6s1r5 foreign key (BUSSINESSRULE_ID) references THO7_2013_2A_TEAM4.BRG_BUSINESSRULE
+//		3603 ERROR SchemaUpdate       - ORA-02298: Kan (THO7_2013_2A_TEAM4.FK_2XLUAIV9BR64GPIUOQYF6S1R5) niet valideren - bovenliggende sleutels zijn niet gevonden.
 //
-//		s.save(persistable); // This would save the object in session
-//		s.flush(); // SQL Query is generated
-//		tx.commit(); // Commits the transaction
-//		s.close(); // Session is closed
+//		3699 ERROR SchemaUpdate       - HHH000388: Unsuccessful: alter table THO7_2013_2A_TEAM4.BRG_VALUE add constraint FK_5llnfy2yffbi6vrmxvd13ea5x foreign key (BUSINESSRULE_ID) references THO7_2013_2A_TEAM4.BRG_BUSINESSRULE
+//		3699 ERROR SchemaUpdate       - ORA-02298: Kan (THO7_2013_2A_TEAM4.FK_5LLNFY2YFFBI6VRMXVD13EA5X) niet valideren - bovenliggende sleutels zijn niet gevonden.
 	}
-	
-//	/* Method to list all the employees detail */
-//	public void listEmployees( ){
-//		Session session = factory.openSession();
-//		Transaction tx = null;
-//	
-//		try{
-//			tx = session.beginTransaction();
-//			List employees = session.createQuery("FROM Employee").list();
-//			for (Iterator iterator = employees.iterator(); iterator.hasNext();){
-//				Employee employee = (Employee) iterator.next();
-//				System.out.print("First Name: " + employee.getFirstName());
-//				System.out.print(" Last Name: " + employee.getLastName());
-//				System.out.println(" Salary: " + employee.getSalary());
-//				Address add = employee.getAddress();
-//				System.out.println("Address ");
-//				System.out.println("\tStreet: " + add.getStreet());
-//				System.out.println("\tCity: " + add.getCity());
-//				System.out.println("\tState: " + add.getState());
-//				System.out.println("\tZipcode: " + add.getZipcode());
-//			}
-//			tx.commit();
-//		}catch (HibernateException e) {
-//		if (tx!=null) tx.rollback();
-//			e.printStackTrace();
-//		}finally {
-//			session.close();
-//		}
-//	}
 }
