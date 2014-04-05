b88d46b617de09f9e4f97c50ed362490750ca09e
smartlapus <smartlapus@gmail.com>
2014-04-05 22:59:46 +0200

    launcher todo's

--- businessRuleGenerator/src/Launcher.java
+++ businessRuleGenerator/src/Launcher.java
@@ -1,19 +1,7 @@
-import java.util.ArrayList;
-
-import javax.transaction.Transactional;
-
-import org.hibernate.Hibernate;
-import org.hibernate.HibernateException;
-import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
-import org.hibernate.cfg.Configuration;
 
 import com.r00lerz.businessRuleGenerator.domain.HibernateUtil;
-import com.r00lerz.businessRuleGenerator.domain.datamodel.Application;
-import com.r00lerz.businessRuleGenerator.domain.datamodel.BackEndRuleType;
-import com.r00lerz.businessRuleGenerator.domain.datamodel.BusinessRule;
-import com.r00lerz.businessRuleGenerator.domain.datamodel.Dao.BackEndRuleTypeDAO;
+import com.r00lerz.businessRuleGenerator.domain.datamodel.Value;
 
 public class Launcher {
 	
@@ -24,20 +12,31 @@ public class Launcher {
 //		3699 ERROR SchemaUpdate       - HHH000388: Unsuccessful: alter table THO7_2013_2A_TEAM4.BRG_VALUE add constraint FK_5llnfy2yffbi6vrmxvd13ea5x foreign key (BUSINESSRULE_ID) references THO7_2013_2A_TEAM4.BRG_BUSINESSRULE
 //		3699 ERROR SchemaUpdate       - ORA-02298: Kan (THO7_2013_2A_TEAM4.FK_5LLNFY2YFFBI6VRMXVD13EA5X) niet valideren - bovenliggende sleutels zijn niet gevonden.
 		
-		SessionFactory sf = HibernateUtil.configureSessionFactory(); 
-				
-		sf.getCurrentSession().beginTransaction();  
+//		SessionFactory sf = HibernateUtil.configureSessionFactory(); 
+//		sf.getCurrentSession().beginTransaction();  
 		
-		BackEndRuleType bert;
-		bert = new BackEndRuleTypeDAO().retrieveTypeByName("Compare Rule");
-        
-		System.out.println("FOUND: " + bert);
+		
+			
+		
+		// TODO: remove generated files after completion
+		// TODO: randomized code generation
+		// TODO: mapping for status, mapping for table name a business rule applies to
+		// TODO: documentation and testing -- SALES PITCH!
+		
+		// TODOCUMENT :: open session in view pattern.
+		// TODOCUMENT :: power of hibernate & open-source options because of it.
+		
+		
+		
+		
+		        
 
-         // Commit and cleanup  
-         System.out.println("HIBERNATESESSIONFILTER::TEST");
-         
-         // sf.getCurrentSession().getTransaction().commit();
-         sf.getCurrentSession().close();
+
+//         // Commit and cleanup  
+//         System.out.println("HIBERNATESESSIONFILTER::TEST");
+//         
+//         // sf.getCurrentSession().getTransaction().commit();
+//         sf.getCurrentSession().close();
 		
 		
 		
