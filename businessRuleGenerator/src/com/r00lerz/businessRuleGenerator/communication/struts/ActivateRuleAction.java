c240e2ab50b040ad68ae05734b74f1dbaa593576
Robin Altena <altenarobin@gmail.com>
2014-04-09 13:54:05 +0200

    implemented GSON error handling in ActivateRuleAction

--- businessRuleGenerator/src/com/r00lerz/businessRuleGenerator/communication/struts/ActivateRuleAction.java
+++ businessRuleGenerator/src/com/r00lerz/businessRuleGenerator/communication/struts/ActivateRuleAction.java
@@ -1,29 +1,44 @@
 package com.r00lerz.businessRuleGenerator.communication.struts;
 
-import org.apache.struts2.ServletActionContext;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+import org.apache.struts2.ServletActionContext;
+import org.eclipse.xtext.validation.Issue;
+
+import com.google.gson.Gson;
 import com.opensymphony.xwork2.ActionSupport;
 import com.r00lerz.businessRuleGenerator.domain.BrgService;
 import com.r00lerz.businessRuleGenerator.domain.BrgServiceImpl;
 import com.r00lerz.ruleDef.RuleDefException;
 
-public class ActivateRuleAction extends ActionSupport { // implements UserAware
-
+public class ActivateRuleAction extends ActionSupport {
 	// names of parameters here
 	private int id;
+	private String result;
 
 	@Override
 	public String execute() {
 		String realPath = ServletActionContext.getServletContext().getRealPath("/");
 		BrgService service = BrgServiceImpl.getService();
-		
-		String result = "";
+
+		Map<String, Object> result = new HashMap<String, Object>();
 		try {//TODO::THINK OF A WAY TO HANDLE THE EXCEPTION
 			service.activeBusinessRule(id, realPath);
+			result.put("success", id);
 		} catch (RuleDefException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
+			List<String> errors = new ArrayList<String>();
+			for (Issue issue : e.getIssues()){
+				System.out.println(issue);
+				errors.add(issue.getMessage());
+			}
+			result.put("errors", errors);
 		}
+		Gson gson = new Gson();
+		this.result = gson.toJson(result);
 		return ActionSupport.SUCCESS;
 	}
 
